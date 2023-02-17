33305a3dec5c32c6415a9db2cc192a9638f6826e
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-02-17 21:43:22 +0300

    Registered factories

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,6 +3,7 @@ from pytest_factoryboy import register
 import tests.factories
 
 register(tests.factories.UserFactory)
-
+register(tests.factories.ProductFactory)
+register(tests.factories.ProviderFactory)
 
 pytest_plugins = 'tests.fixtures'
