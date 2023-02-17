2ed61853550f0018d2179b2584364ca345b980bc
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2023-02-17 21:42:31 +0300

    Created link for provider

--- backend/providers/admin.py
+++ backend/providers/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from providers.models import Product, Provider
 
@@ -11,5 +13,25 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(Provider)
 class ProviderAdmin(admin.ModelAdmin):
-    list_display = ('title', 'type', 'country', 'city', 'date_created', 'id')
+    list_display = ('title', 'type', 'country', 'city', 'debts', 'provider_link', 'id')
     list_per_page = 20
+    list_filter = ('city', 'type')
+    actions = ('clear_debts', )
+
+    def clear_debts(self, request, queryset):
+        for obj in queryset:
+            obj.debts = 0
+            obj.save()
+
+    clear_debts.short_description = 'Очистить задолженность'
+
+    def provider_link(self, obj):
+
+        if obj.provider:
+            link = reverse(
+                'admin:providers_provider_change',
+                args=(obj.provider.id,)
+            )
+            return mark_safe(
+                u"<a href='{0}'>{1}</a>".format(link, obj.provider)
+            )
---
+++ backend/users/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "provider_link")
+    search_fields = ("username", "email", "first_name", "last_name")
+    readonly_fields = ("last_login", "date_joined")
+    fieldsets = (
+        (None, {"fields": ("username", "password", "provider")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "email")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    def provider_link(self, obj):
+
+        if obj.provider:
+            link = reverse(
+                'admin:providers_provider_change',
+                args=(obj.provider.id,)
+            )
+            return mark_safe(
+                u"<a href='{0}'>{1}</a>".format(link, obj.provider)
+            )
